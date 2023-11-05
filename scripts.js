594112505d83d65055c82b77d114e301d6fd5713
mynumu <121490973+mynumu@users.noreply.github.com>
2023-11-05 18:08:48 +0000

    change VF bot

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const voiceflowVersionID =
     document.getElementById('vfassistant').getAttribute('data-version') ||
     'development'
-  const voiceflowAPIKey = 'VF.DM.654667017e31c10008fce157.dgtzwbjPs6F0xKAQ'
+  const voiceflowAPIKey = 'VF.DM.6546546144035a00075815f7.rEeWBQnU8inMqTS0'
 
   let audio = new Audio()
   const wave = document.getElementById('wave')
